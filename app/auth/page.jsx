34e93517d74225231486ea2845a61a15cb1ffb19
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-04 00:15:46 +0530

    update the login page

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -6,11 +6,13 @@ import Img from '../../public/login.jpg';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+
 
 function Login() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className='flex flex-col items-center gap-6'>
+    <div className='flex justify-center items-center h-screen mt-10 mb-10'>
+      <div className='flex flex-col items-center gap-6 border rounded-2xl p-8'>
         
         <div className='flex items-center gap-3 relative'>
           <motion.div
@@ -31,7 +33,7 @@ function Login() {
           />
         </div>
 
-        <div>
+        <div className='flex flex-col items-center'>
           <Image 
             src={Img} 
             alt="login" 
@@ -41,6 +43,7 @@ function Login() {
           />
           <h2 className='text-2xl font-bold text-center mt-5'>Welcome to Voxicruit</h2>
           <p className='text-gray-500 text-center'>Sign In With Google Authentication</p>
+          <Button className={'mt-7 w-full'}>Login with Google</Button>
         </div>
       </div>
     </div>
