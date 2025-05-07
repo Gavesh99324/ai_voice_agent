9f618c20053e9c99f40f7a3680ac7983532e51ac
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-07 13:02:23 +0530

    add user authorization image in the dashboard header

--- app/(main)/dashboard/_components/WelcomeContainer.jsx
+++ app/(main)/dashboard/_components/WelcomeContainer.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { useUser } from '@/app/provider';
+import Image from 'next/image';
 
 function WelcomeContainer() {
 
@@ -11,12 +12,12 @@ function WelcomeContainer() {
 
 
   return (
-    <div>
-      <div className={"p-2 rounded-xl w-full"} style={{ backgroundColor: 'rgba(163, 235, 30, 0.4)' }}>
+    <div className={"p-2 rounded-xl w-full flex justify-between items-center"} style={{ backgroundColor: 'rgba(163, 235, 30, 0.4)' }}>
+      <div>
         <h2 className={"text-lg font-bold"}>Welcome Back, {user?.name || 'Guest'}</h2>
         <h2 className={"text-gray-500"}>AI-Driven Interviews, Simplified, Hustle-Free Hiring</h2>
       </div>
-      
+      {user&& <Image className="rounded-full" src={user?.picture} width={50} height={50} alt={'Welcome Image'} />}
     </div>
   )
 }
