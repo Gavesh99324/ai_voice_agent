f69c66be342bd393abf90be563a10230369777d8
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-09 00:31:11 +0530

    use gemini-2.5-pro model

--- app/api/ai-model/route.jsx
+++ app/api/ai-model/route.jsx
@@ -2,9 +2,21 @@ import { OpenAI } from "openai";
 
 export async function POST(req) {
 
+    const {jobPosition, jobDescription, duration, InterviewType} = await req.json();
+
+
   const openai = new OpenAI({
      baseURL: "https://openrouter.ai/api/v1",
      apiKey: process.env.OPENROUTER_API_KEY,
-  })  
+  })
+
+  const completion = await openai.chat.completions.create({
+    model: "google/gemini-2.5-pro-exp-03-25",
+    messages: [
+      { role: "user", content: "Say this is a test" }
+    ],
+  })
+  console.log(completion.choices[0].message)
+
 }
 
