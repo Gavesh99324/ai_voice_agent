85a903270e027a967765e589dc34b80bf9830a72
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-09 19:46:26 +0530

    make showing the generating questions

--- app/(main)/dashboard/create-interview/_components/QuestionList.jsx
+++ app/(main)/dashboard/create-interview/_components/QuestionList.jsx
@@ -23,16 +23,23 @@ function QuestionList({ formData }) {
         ...formData
       });
       console.log(result.data.content);
-      const Content = JSON.parse(result.data.content);
-      setQuestionList(Content);
+
+      let contentString = result.data.content.trim();
+
+     if (contentString.startsWith("```json")) {
+        contentString = contentString.replace("```json", "").replace("```", "").trim();
+     }
+
+     const Content = JSON.parse(contentString);
+
+      setQuestionList(Content?.interviewQuestions);
       setLoading(false);
 
     } catch (error) {
       toast('Server Error, Try Again');
       setLoading(false);
-    }
-
-    }
+  }
+}
  
   return (
     <div>
@@ -45,6 +52,17 @@ function QuestionList({ formData }) {
         </div>
       </div>
       }
+
+      {questionList?.length > 0 &&
+        <div className={"p-5 border border-gray-300 rounded-xl"}>
+          {questionList.map((item, index) => (
+            <div key={index} className={"p-3 border border-gray-200 rounded-xl"}>
+              <h2 className={"font-medium"}>{item.question}</h2>
+              <h2>Type: {item.type}</h2>
+            </div>
+          ))}
+        </div>
+        } 
     </div>
   )
 }
