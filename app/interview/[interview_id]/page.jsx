32bbd9641465df45177e498b5c57cf75c93fd348
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-12 13:13:57 +0530

    create interview page

--- app/interview/[interview_id]/page.jsx
+++ app/interview/[interview_id]/page.jsx
@@ -1,10 +1,30 @@
+"use client";
+
 import React from 'react';
 import InterviewHeader from '../_components/InterviewHeader';
+import { motion } from 'framer-motion';
+import { IoIosMic } from "react-icons/io";
+import { TypeAnimation } from 'react-type-animation';
 
 function Interview() {
   return (
-    <div>
-     {/* <InterviewHeader /> */}
+    <div className={"px-10 md:px-28 lg:px-48 xl:px-64 mt-16"}>
+      <div className={"flex justify-center border rounded-xl"}>
+        <div className='flex items-center gap-3 relative'>
+
+          <IoIosMic size={30} color="#A3EB1E" className="relative z-10" />
+
+          <TypeAnimation
+            sequence={['Voxicruit']}
+            wrapper="span"
+            speed={20}
+            style={{ fontSize: '20px', fontWeight: 'bold', display: 'inline-block' }}
+            repeat={0}
+            cursor={false}
+          />
+          <span className="blinking-dot">.</span>
+        </div>
+      </div>
     </div>
   )
 }
--- app/interview/_components/InterviewHeader.jsx
+++ app/interview/_components/InterviewHeader.jsx
@@ -1,14 +1,34 @@
+'use client';
+
 import React from 'react';
-import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { IoIosMic } from "react-icons/io";
+import { TypeAnimation } from 'react-type-animation';
 
 function InterviewHeader() {
   return (
-    <div>
-      <Image src={""} width={100} height={100} alt='logo'
-      className={"w-[140]"} 
-       />
+    <div className={"p-4 shadow-sm"}>
+      <div className='flex items-center gap-3 relative'>
+          <motion.div
+            className="absolute w-11 h-11 rounded-full border-2 border-green-300"
+            animate={{ scale: [1, 1.5, 2], opacity: [1, 0.6, 0] }}
+            transition={{ repeat: Infinity, duration: 2, ease: "easeOut" }}
+          />
+
+          <IoIosMic size={50} color="#A3EB1E" className="relative z-10" />
+
+          <TypeAnimation
+            sequence={['Voxicruit']}
+            wrapper="span"
+            speed={20}
+            style={{ fontSize: '30px', fontWeight: 'bold', display: 'inline-block' }}
+            repeat={0}
+            cursor={false}
+          />
+          <span className="blinking-dot">.</span>
+        </div>
     </div>
   )
-}
+};
 
 export default InterviewHeader;
