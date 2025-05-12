3996afaa8c4f42037bd21c185d5a49eae24d578d
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-12 23:13:13 +0530

    update

--- app/interview/[interview_id]/page.jsx
+++ app/interview/[interview_id]/page.jsx
@@ -1,18 +1,56 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import InterviewHeader from '../_components/InterviewHeader';
 import { motion } from 'framer-motion';
 import { IoIosMic } from "react-icons/io";
 import { TypeAnimation } from 'react-type-animation';
 import Image from 'next/image';
-import { Clock } from 'lucide-react';
+import { Clock, Info, Video } from 'lucide-react';
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { useParams } from 'next/navigation';
+import { supabase } from '@/services/supabaseClient';
+import { toast } from 'sonner';
 
 function Interview() {
+
+  const {interview_id} = useParams();
+  console.log(interview_id);
+
+  useEffect(() => {
+    interview_id && GetInterviewDetails();
+  }, [interview_id]);
+
+  const [interviewData, setInterviewData] = useState();
+  const [userName, setUserName] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const GetInterviewDetails = async () => {
+    setLoading(true); 
+    try{
+    let { data: Interviews, error } = await supabase
+      .from('Interviews')
+      .select("jobPosition, jobDescription, duration, type")
+      .eq('interview_id', interview_id);
+
+      setInterviewData(Interviews[0]);
+      if (Interview?.length == 0) {
+        toast('Incorrect Interview Link');
+        return;
+      }
+      console.log(Interviews[0]);
+
+    } catch (e) {
+      setLoading(false);
+      toast('Incorrect Interview Link');
+    }
+  }
+
+
   return (
-    <div className={"px-10 md:px-28 lg:px-48 xl:px-64 mt-16"}>
-      <div className={"flex flex-col items-center justify-center border rounded-xl bg-white p-5 lg:px-52"}>
+    <div className={"px-10 md:px-28 lg:px-40 xl:px-64 mt-16 mb-20"}>
+      <div className={"flex flex-col items-center justify-center border rounded-xl bg-white p-5 lg:px-52 mb-20"}>
         <div className='flex items-center gap-3 relative'>
 
           <IoIosMic size={30} color="#A3EB1E" className="relative z-10" />
@@ -30,8 +68,8 @@ function Interview() {
         <h2 className={"mt-3"}>AI-Powered Interview Platform</h2>
         <Image src={'/Int1.png'} width={300} height={200} alt={"interview"} />
 
-        <h2 className={"font-bold text-xl mt-3"}>Full stack developer interview</h2>
-        <h2 className={"flex gap-2 items-center text-gray-400 mt-3"}><Clock className={"w-4 h-4"} /> 30 min</h2>
+        <h2 className={"font-bold text-xl mt-3"}>{interviewData?.jobPosition}</h2>
+        <h2 className={"flex gap-2 items-center text-gray-400 mt-3"}><Clock className={"w-4 h-4"} /> {interviewData?.duration}</h2>
 
         <div className={"w-full"}>
           <h2>Enter Full Name</h2>
@@ -47,6 +85,8 @@ function Interview() {
             <li className={"text-sm text-primary"}>- Find a Quiet place for interview</li>
           </ul>
         </div>
+
+        <Button className={"mt-5 w-full font-bold"}><Video/>Join Interview</Button>
       </div>
     </div>
   )
