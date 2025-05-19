46799f1ff77b515fb19a316063227e5ccb8fc9b9
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-19 18:36:20 +0530

    update

--- app/(main)/scheduled-interview/[interview_id]/_components/InterviewDetailContainer.jsx
+++ app/(main)/scheduled-interview/[interview_id]/_components/InterviewDetailContainer.jsx
@@ -7,21 +7,30 @@ function InterviewDetailContainer({ interviewDetail }) {
 
   return (
     <div className={"p-5 bg-white rounded-lg mt-5"}>
-      <h2>{interviewDetail?.jobPosition}</h2>
+      <h2 className={"text-green-500"}>{interviewDetail?.jobPosition}</h2>
 
-      <div className={"mt-4 flex justify-between items-center"}>
+      <div className={"mt-4 flex justify-between items-center lg:pr-25"}>
         <div>
             <h2 className={"text-xs text-gray-600"}>Duration</h2>
-            <h2 className={"text-sm font-bold flex items-center gap-2"}><Clock className={"h-4 w-4"} /> {interviewDetail?.duration}</h2>
+            <h2 className={"text-sm font-bold flex items-center gap-2 mt-2"}><Clock className={"h-4 w-4"} /> {interviewDetail?.duration}</h2>
         </div>
+
         <div>
             <h2 className={"text-xs text-gray-600"}>Created On</h2>
-            <h2 className={"text-sm font-bold flex items-center gap-2"}><Calendar className={"h-4 w-4"} /> {moment(interviewDetail?.created_at).format('MMMM Do YYYY')}</h2>
+            <h2 className={"text-sm font-bold flex items-center gap-2 mt-2"}><Calendar className={"h-4 w-4"} /> {moment(interviewDetail?.created_at).format('MMMM Do YYYY')}</h2>
         </div>
+
+        {interviewDetail?.type &&
         <div>
             <h2 className={"text-xs text-gray-600"}>Type</h2>
-            <h2 className={"text-sm font-bold flex items-center gap-2"}><Clock className={"h-4 w-4"} /> {interviewDetail?.type}</h2>
+            <h2 className={"text-sm font-bold flex items-center gap-2 mt-2"}><Clock className={"h-4 w-4"} /> {JSON.parse(interviewDetail?.type)}</h2>
         </div>
+        }
+      </div>
+
+      <div>
+        <h2 className={"font-bold mt-5"}>Job Description</h2>
+        <p className={"mt-2"}>{interviewDetail?.jobDescription}</p>
       </div>
     </div>
   )
